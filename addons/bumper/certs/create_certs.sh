a3bae5ef0e84a87e324ece65a53aa35dfb96f2b3
Stephen Ball <stephen@rebelinblue.com>
2025-02-19 22:18:59 +0000

    Update bumper

--- addons/bumper/certs/create_certs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-openssl genrsa -out ca.key 4096
-openssl req -new -nodes -key ca.key -config csrconfig_ca.txt -out ca.csr
-openssl req -x509 -nodes -in ca.csr -days 1095 -key ca.key -config certconfig_ca.txt -extensions req_ext -out ca.crt
-
-openssl genrsa -out bumper.key 4096
-openssl req -new -nodes -key bumper.key -config csrconfig_bumper.txt -out bumper.csr
-openssl x509 -req -in bumper.csr -days 365 -CA ca.crt -CAkey ca.key -extfile certconfig_bumper.txt -extensions req_ext -CAcreateserial -out bumper.crt
